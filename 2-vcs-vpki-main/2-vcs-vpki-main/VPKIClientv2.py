1d12d8fc8538b63ebfdeeda5569fba37031baae4
mohamaddib147 <122824829+mohamaddib147@users.noreply.github.com>
2023-10-05 14:17:15 +0200

    Add files via upload

--- 2-vcs-vpki-main/2-vcs-vpki-main/VPKIClientv2.py
+++ 2-vcs-vpki-main/2-vcs-vpki-main/VPKIClientv2.py
@@ -8,13 +8,15 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
-from interfaces_pb2 import msgX509CertReq_V2LTCA, msgTicketReq, msgPsnymCertReq_V2PCA
+from interfaces_pb2 import msgX509CertReq_V2LTCA, msgTicketReq, msgPsnymCertReq_V2PCA,msgX509CertReq_V2LTCA
+LTCA_METHOD_NAME = "ltca.operate"
+PCA_METHOD_NAME = "pca.operate"
 
 class VPKIClient:
-    def __init__(self, ltca_url, pca_url, client_cert_path, client_key_path, validate_certificate=False):
+    def __init__(self, ltca_url, pca_url, csr_file_path, client_key_path, validate_certificate=False):
         self.ltca_url = ltca_url
         self.pca_url = pca_url
-        self.client_cert_path = client_cert_path
+        self.csr_file_path = csr_file_path
         self.client_key_path = client_key_path
         self.validate_certificate = validate_certificate
 
@@ -22,10 +24,28 @@ class VPKIClient:
         with open(private_key_path, 'rb') as key_file:
             private_key = serialization.load_pem_private_key(key_file.read(), password=None)
         signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
+        
         return signature
+   
+    def send_request(self, url, data, method_name):
+        # Send the request to the server, without verifying the certificate (validate_certificate=False)
+        response = requests.post(url, data=data, verify=self.validate_certificate, headers={"METHOD_NAME": method_name})
 
-    def obtain_ltca_certificate(self):
+        if response.status_code == 200:
+            # Parse and return the response
+            response_message = msgX509CertReq_V2LTCA()
+            response_message.ParseFromString(response.content)
+            return response_message
+        else:
+            raise Exception(f"Error: {response.status_code} - {response.text}")
+
+
+    def obtain_ltca(self):
         try:
+            # Read the CSR from the CSR file
+            with open(csr_file_path, 'rb') as csr_file:
+                csr_data = csr_file.read()
+
             # Create an LTCA request
             ltca_request = msgX509CertReq_V2LTCA()
             ltca_request.iReqType = 122
@@ -34,11 +54,13 @@ class VPKIClient:
             ltca_request.strDNSExtension = ""
             ltca_request.strKeyUsage = ""
             ltca_request.strExtendedKeyUsage = "clientAuth"
-            ltca_request.strX509CertReq = ""  # Leave empty for LTCA request
+            ltca_request.strX509CertReq = csr_data  # Include the CSR data
             ltca_request.iNonce = random.randint(0, 65535)
             ltca_request.tTimeStamp = int(time.time())
-
-            # Sign the LTCA request with the client certificate
+            print("LTCA Request Data:")
+            print(ltca_request.SerializeToString())
+            
+            # Sign the LTCA request 
             signature = self.sign_request(ltca_request.SerializeToString(), self.client_key_path)
             ltca_request.stSign.signature = signature
 
@@ -46,7 +68,9 @@ class VPKIClient:
             ltca_request_string = ltca_request.SerializeToString()
 
             # Send the LTCA request to the LTCA server
-            ltca_response = self.send_request(self.ltca_url, ltca_request_string)
+            ltca_response = msgX509CertReq_V2LTCA()
+            ltca_response.ParseFromString(self.send_request(self.ltca_url, ltca_request_string, self.LTCA_METHOD_NAME))
+
 
             # Handle the response from the LTCA server
             ltca_certificate = ltca_response.strX509Cert  # Extract the LTCA certificate
@@ -74,8 +98,8 @@ class VPKIClient:
             ticket_request_string = ticket_request.SerializeToString()
 
             # Send the ticket request to the LTCA server
-            ticket_response = self.send_request(self.ltca_url, ticket_request_string)
-
+            ticket_response = msgTicketReq()
+            ticket_response.ParseFromString(self.send_request(self.ltca_url, ticket_request_string, self.LTCA_METHOD_NAME))
             # Handle the response from the LTCA server
             ticket = ticket_response.strTicket  # Extract the ticket
 
@@ -101,7 +125,8 @@ class VPKIClient:
             pseudonym_request_string = pseudonym_request.SerializeToString()
 
             # Send the pseudonym request to the PCA server
-            pseudonym_response = self.send_request(self.pca_url, pseudonym_request_string)
+            pseudonym_response = msgPsnymCertReq_V2PCA()
+            pseudonym_response.ParseFromString(self.send_request(self.pca_url, pseudonym_request_string,self.PCA_METHOD_NAME))
 
             # Handle the response from the PCA server
             pseudonym_certificate = pseudonym_response.stPsnymCert  # Extract the pseudonym certificate
@@ -110,31 +135,20 @@ class VPKIClient:
 
         except Exception as e:
             raise Exception(f"Error: {str(e)}")
-
-    def send_request(self, url, data):
-        # Send the request to the server, without verifying the certificate (validate_certificate=False)
-        response = requests.post(url, data=data, verify=self.validate_certificate)
-
-        if response.status_code == 200:
-            # Parse and return the response
-            response_message = msgX509CertReq_V2LTCA()
-            response_message.ParseFromString(response.content)
-            return response_message
-        else:
-            raise Exception(f"Error: {response.status_code} - {response.text}")
-
+ 
+    
 if __name__ == "__main__":
     ltca_url = "https://nss-core.ddns.net:30930/cgi-bin/ltca"
     pca_url = "https://nss-core.ddns.net:30931/cgi-bin/pca"
 
-    # Update the client_cert_path and client_key_path variables to match the paths to your client certificate and key files.
-    client_cert_path = "/home/kali/Desktop/CSD/2-vcs-vpki-main/psnym.crt"
-    client_key_path = "/home/kali/Desktop/CSD/2-vcs-vpki-main/new_key.pem"
-    csr_file_path = "/home/kali/Desktop/CSD/2-vcs-vpki-main/new_csr.pem"
-    vpkiclient = VPKIClient(ltca_url, pca_url, client_cert_path, client_key_path, validate_certificate=False)
+    # Update the csr_file_path and client_key_path variables to match the paths to your client certificate and key files.
+    
+    client_key_path = "/home/kali/Desktop/CSD/2-vcs-vpki-main/client_key.pem"
+    csr_file_path = "/home/kali/Desktop/CSD/2-vcs-vpki-main/client_csr.pem"
+    vpkiclient = VPKIClient(ltca_url, pca_url, csr_file_path, client_key_path, validate_certificate=False)
 
     try:
-        ltca_certificate = vpkiclient.obtain_ltca_certificate()
+        ltca_certificate = vpkiclient.obtain_ltca()
         ticket = vpkiclient.obtain_ticket(ltca_certificate)
         pseudonym_certificate = vpkiclient.obtain_pseudonym(ticket)
 
@@ -143,3 +157,4 @@ if __name__ == "__main__":
         print(f"Obtained Pseudonym Certificate: {pseudonym_certificate}")
     except Exception as e:
         print(f"Error: {str(e)}")
+#validate the nonce and the time stamp
